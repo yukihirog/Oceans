396a39a7879f69fdea23809035ebbfa4708f91ad
yukihirog <yukihirog@oom-ltd.com>
2013-12-04 20:51:07 +0900

    oneを追加

--- Oceans/EventInterface/EventInterface.js
+++ Oceans/EventInterface/EventInterface.js
@@ -5,6 +5,9 @@ Oceans.EventInterface = function () {
 Oceans.EventInterface.prototype.on = function (type, func) {
 	jQuery(this).on(type, func);
 };
+Oceans.EventInterface.prototype.one = function (type, func) {
+	jQuery(this).one(type, func);
+};
 Oceans.EventInterface.prototype.off = function (type, func) {
 	jQuery(this).off(type, func);
 };
